800dbdd26df06ed6f387af1e7e5c1efee9dd67fd
Mark McCahill <mark.mccahill@duke.edu>
2016-06-30 11:54:01 -0400

    precompile assets code
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,24 @@ module Synthetic
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.01'
+    
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
+    
+    
   end
 end
