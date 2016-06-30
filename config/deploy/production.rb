0ec69e63837f1fc0e45f69dc564aa78603406872
Mark McCahill <mark.mccahill@duke.edu>
2016-06-30 10:26:18 -0400

    asset pipeline
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,11 @@
 server 'synthetic-web-01.oit.duke.edu', :app, :web, :db, :primary => true
 set :rails_env, 'production'
-# Deploy via git repository
-#set :deploy_via, :export
+set :deploy_via, :export
 # Deploy from local working copy
-set :repository, "."
-set :scm, :none
-set :deploy_via, :copy
-set :copy_exclude, [".git/*", 'config/database.yml', 'config/synthetic-creds.yml']
+#set :repository, "."
+#set :scm, :none
+#set :deploy_via, :copy
+#set :copy_exclude, [".git/*", 'config/database.yml', 'config/dukeapps-creds.yml']
 
 set :vhost, 'synthetic.oit.duke.edu'
 
